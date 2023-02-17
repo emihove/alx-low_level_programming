b2b9d668c4897de70bb2434064ebc08f227e1927
emihove <emihove@gmail.com>
2023-02-16 17:17:25 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/** main - A program that prints a line to the standard error
+ *  Return:0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;         
+long long int d;
+float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
