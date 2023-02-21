a5cd1d7f047cccae0752f6473b79156203fff89e
emihove <emihove@gmail.com>
2023-02-21 15:58:05 -0800

    alpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * isalpha - checks for alphabetic character
- * c - the character to be checked
+ * _islower - checks for alphabetic character
+ * @c - the character to be checked
  * Return: 1 for lowercase or 0 for anything else
  */
 int _islower(int c)
