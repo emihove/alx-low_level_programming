0eaea5d1471d200376cc421828a0e999ce36c7e1
emihove <emihove@gmail.com>
2023-02-21 15:22:38 -0800

    alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
  * print_alphabet_x10 - Make alphabet 10 times
  * Return: void
  */
@@ -14,8 +14,7 @@ void print_alphabet_x10(void)
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-		} 
+		}
 			_putchar('\n');
 			i++;
-	}
-} 
+	} }
