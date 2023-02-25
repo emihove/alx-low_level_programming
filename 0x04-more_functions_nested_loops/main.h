e1fc5cb8983fd49ac21e8634e409efa3265b8e8a
emihove <emihove@gmail.com>
2023-02-25 00:28:53 -0800

    upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,7 @@
-#ifndef MAIN.H
-define MAIN.H
-
-int isupper(int c);
-int isdigit(int c);
+int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
@@ -13,5 +9,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif
+int _putchar(char c);
