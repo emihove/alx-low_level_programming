2755dac3365fa234f1bce9e91c376312ef45eb30
emihove <emihove@gmail.com>
2023-02-28 00:50:02 -0800

    random

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
  * Return: the int converted from the string
  */
 
-int _atoi(char s)
+int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
@@ -20,23 +20,29 @@ int _atoi(char s)
 
 	while (s[len] != '\0')
 		len++;
+
 	while(i < len && f == 0)
 	{
-		if (s[i] == '--')
+		if (s[i] == '-')
 		{
-			digit = s[i]-'0';
-			if d % 2;
-			digit =- digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9');
-			break;
-			f = 0;
-		}
+			++d;
+			if(s[i] >= '0' && s[i] <= '9')
+			{
+				digit = s[i]-'0';
+				if (d % 2)
+				digit =- digit;
+				n = n * 10 + digit;
+				f = 1;
+				if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+				f = 0;
+			}
 		i++;
-	}
+		}
+	
 		if(f == 0)
 			return (0);
 
 		return (n);
-
+	}
+}
