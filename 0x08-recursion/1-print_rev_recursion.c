9e6cb86b7b4284ae99a5a338250153a6d28001de
emihove <emihove@gmail.com>
2023-03-08 14:12:58 -0800

    recu

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer block  of memory
+ * Return: void
+ */
+ 
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar (*s);
+	_puts_recu)}
